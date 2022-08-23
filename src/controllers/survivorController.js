80d5d9d35c637e2911bc64ae131ffa2e6235424b
SnowSlurpie <runniale000@gmail.com>
2022-08-23 17:09:39 -0500

    added controllers

---
+++ src/controllers/survivorController.js
@@ -0,0 +1,9 @@
+const factory = require("./handlerFactory");
+const Survivor = require("./../models/survivorModel");
+const SurvivorPerk = require("./../models/survivorPerk");
+
+exports.getAllSurvs = factory.getAll(Survivor);
+exports.getRandomSurv = factory.getRandom(Survivor, 1);
+exports.getRandomSurvs = factory.getRandom(Survivor, 5);
+exports.getSurv = factory.getOne(Survivor);
+exports.getSurvPerks = factory.getOnesPerks(SurvivorPerk, "survivor");
