7a19e74f7294c302b95f1487c85324fc207c77c2
SnowSlurpie <runniale000@gmail.com>
2022-09-14 18:02:47 -0600

    changed structure and created characters

--- src/controllers/survivorController.js
+++
@@ -1,9 +0,0 @@
-const factory = require("./handlerFactory");
-const Survivor = require("./../models/survivorModel");
-const SurvivorPerk = require("./../models/survivorPerk");
-
-exports.getAllSurvs = factory.getAll(Survivor);
-exports.getRandomSurv = factory.getRandom(Survivor, 1);
-exports.getRandomSurvs = factory.getRandom(Survivor, 5);
-exports.getSurv = factory.getOne(Survivor);
-exports.getSurvPerks = factory.getOnesPerks(SurvivorPerk, "survivor");
