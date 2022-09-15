31b08f2c1ae981c46db3d96abbcbc443385d0884
SnowSlurpie <runniale000@gmail.com>
2022-09-14 19:08:16 -0600

    added home and index

---
+++ src/pages/home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {AccordionTab} from 'primereact';
+
+<AccordionTab>
+    test
+</AccordionTab>
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Router from '@reach/router';
+
+
+const IndexRouter = () => (
+    <Router>
+        <Homepage path="/" />
+    </Router>
+)
+
+
+export default IndexRouter;
